c458b58824c0f3d3fa310e5dc1fb44f145c031a7
Maniixer <maniixer@hotmail.com>
2024-02-12 09:37:01 +0100

    css updates

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,7 @@
 export function Skill({ name, image }) {
   return (
     <div className="skill">
-      <h3>{name}</h3>
+      {/* <h3>{name}</h3> */}
       <img src={image} alt={name} />
     </div>
   );
